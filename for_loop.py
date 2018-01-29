73722fa4db0d97c4951df4fbc1d6ab0ec2e298d1
Matt Fox <mdfox760@gmail.com>
2018-01-29 12:37:19 -0800

    Edited for_loop

--- for_loop.py
+++ for_loop.py
@@ -17,3 +17,19 @@ for num in range(2, 10):
 
 else:
     print("I found nothing!")
+
+knights = {'Gallahad': 'the pure', 'Robin': 'the brave'}
+for k, v in knights.items():
+    print(k, v)
+
+# When looping through a sequence, the position index and corresponding
+# value can be retrieved at the same time using teh enumerate() function.
+for i, v in enumerate(['tic', 'tac', 'toe']):
+    print(i, v)
+
+# To loop over two or more sequences at the same time, the entries
+# can be paired with the zip function.
+questions = ['name', 'quest', 'favorite color']
+answers = ['Lancelot', 'the holy grail', 'blue']
+for q, a in zip(questions, answers):
+    print('What is your {0}? It is {1}.'.format(q, a))
